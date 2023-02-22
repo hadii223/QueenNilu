7358ede135919f513435d75c8fea02af5a3d3ca9
hadii223 <124769778+hadii223@users.noreply.github.com>
2023-02-22 11:31:09 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,9 @@
 /*
 ===================================================================================================
-💃 𝗤𝘂𝗲𝗲𝗻 𝗡𝗶𝗹𝘂 𝗕𝗼𝘁 𝗠𝗱 💃
-   ____                           _   _ _ _       
-  / __ \                         | \ | (_) |      
- | |  | |_   _  ___  ___ _ __    |  \| |_| |_   _ 
- | |  | | | | |/ _ \/ _ \ '_ \   | . ` | | | | | |
- | |__| | |_| |  __/  __/ | | |  | |\  | | | |_| |
-  \___\_\\__,_|\___|\___|_| |_|  |_| \_|_|_|\__,_|
+💃 pak cyber team💃
+PAK CYBER TEAM 
                                                   
-                                                  BY Janiya
+                                                  BY CYBER RIVET
 ===================================================================================================
 */
 
@@ -22,9 +17,9 @@ global.READ_MASSAGE = 'false'
 //👇 enable desable Girls Voice Reply
 global.VOICE_REPLY =  'true'
 // You Bots Owner Number 
-global.owner = ['94767438882'] 
+global.owner = ['923077973679'] 
 //👇 Your Caption ( Image Video )
-global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝙽𝙸𝙻𝚄 ²⁰²³'
+global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 PAK CYBER TEAM ²⁰²³'
 //👇 Do you want show time on your bio use "on" want desable use "off"
 global.AUTO_BIO =  'off'
 //👇 Inbox massage block PM block
@@ -36,11 +31,11 @@ global.ANTI_BADWORD =  'true'
 //👇 Your Bot Name
 global.botnma =  'ǫᴜᴇᴇɴ ɴɪʟᴜ' 
 //👇 Your name
-global.ownernma =  'JANIYA' 
+global.ownernma =  'CYBER RIVET' 
 //👇 Sticker package name
 global.packname =  '𝗤𝗨𝗘𝗘𝗡 𝐍𝐈𝐋𝐔' 
 //👇 Sticker Author Name
-global.author =  '_JANIYA_' 
+global.author =  '_CYBER_' 
 //👇 Kick And Auto Delete Group link Senders
 global.antilink =  'false'
 //👇 Inbox Block Massage 
@@ -53,9 +48,9 @@ global.BUTTON1 = '𝙶𝚒𝚝𝚑𝚞𝚋'
 //👇 URL button 2 name
 global.BUTTON2 = '𝚈𝚘𝚞𝚝𝚞𝚋𝚎'
 //👇 Button 1 url 
-global.BUTTON1_URL = 'https://github.com/janithsadanuwan/'
+global.BUTTON1_URL = NOO
 //👇 Button 2 url
-global.BUTTON2_URL = 'https://youtube.com/c/Janithsadanuwan'
+global.BUTTON2_URL = 'https://www.youtube.com/@masoomhacker/about'
 //👇 Your Add Massage 
 global.ADD_MSG = '*✅ Success add Group*' 
 //👇 Your Kick Massage 
@@ -105,9 +100,9 @@ global.NO_BLOCK = ["94767438882"]
 global.zenzapi = '01ABEB1E11'
 global.tiktokthub = 'https://telegra.ph/file/d83742c9e0a076364184e.jpg'
 //other
-global.pemilik = ['94767438882'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94767438882'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = '_Janiya_' // Your name ඔබේ නම 
+global.pemilik = ['923077973679'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['923077973679'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = '_Janiya_' // Your name HADII 
 global.sessionName = 'session'
 global.prefix = ['#','!','.'] 
 global.sp = '🔵'
